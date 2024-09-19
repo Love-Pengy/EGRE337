267d6656ff783651e9fd9eedb9e4f94ab39ddd5d
Brandon Frazier <frazierb2@vcu.edu>
2024-09-19 19:46:00 -0400

    complete homework 2

--- HW2/m2_brandon_frazier.m
+++ HW2/m2_brandon_frazier.m
@@ -7,7 +7,7 @@ stdDev = 2 * 10^-3;
 
 %% PART ONE
 Ptheory = normcdf([11*10^-3 15*10^-3], mu, stdDev);
-Ptheory = Ptheory(2) - Ptheory(1);
+Ptheory = Ptheory(2) - Ptheory(1)
 
 %% PART TWO
 
@@ -16,21 +16,23 @@ delays = stdDev .* randn(1000, 1) + mu;
 %% PART THREE
 
 val = sum((delays < 15 * 10^-3) &  (delays > 11 * 10^-3));
-Pexp = val/numel(delays);
+Pexp = val/numel(delays)
 
 
 %% PART FOUR (tentative)
 
-relError = abs(((Pexp - Ptheory)/Ptheory)) * 100;
+relError = abs(((Pexp - Ptheory)/Ptheory)) * 100
 
 %% PART FIVE (tentative)
 
-hold on 
 % PART A
-
+subplot(2, 1, 1);
 plot(normpdf(linspace(.003, .017), mu, stdDev))
-
+title("PDF")
+xlabel("delays")
+subplot(2, 1, 2);
 % PART B
 histogram(delays)
+title("Delay Histogram")
 
-hold off
+
